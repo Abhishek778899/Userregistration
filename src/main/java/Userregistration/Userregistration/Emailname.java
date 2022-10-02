d670e5c38ba0609b3282b7dec7005809524ec5b5
Abhishek778899 <abhisheksuryawanshi325@gmail.com>
2022-10-02 14:49:34 +0530

    Emailname

---
+++ src/main/java/Userregistration/Userregistration/Emailname.java
@@ -0,0 +1,26 @@
+package Userregistration.Userregistration;
+
+import java.util.regex.Pattern;
+
+public class Emailname {
+
+	private static final String firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+	private static final String lastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*[@]{1}[a-z]+[.]{1}[a-z]{2,4}[.]*[a-z]*{2}$";
+
+	public boolean validateFirstName(String fName) {
+		boolean result = Pattern.matches(firstName, fName);
+		return result;
+	}
+
+	public boolean validateLastName(String lName) {
+		boolean result = Pattern.matches(lastName, lName);
+		return result;
+	}
+
+	public boolean validateEmail(String email) {
+		boolean result = Pattern.matches(EMAIL_PATTERN, email);
+		return result;
+	}
+
+}
